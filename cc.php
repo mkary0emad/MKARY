7b5a006807383ba0dbbd0549c14b33c708f5dfee
mkary0emad <0mkary0emad0@gmail.com>
2025-10-26 09:42:28 +0300

    Add files via upload

--- cc.php
+++ cc.php
@@ -52,7 +52,8 @@ $default_timezone = 'Etc/UTC'; // UTC
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
 $root_path = $_SERVER['DOCUMENT_ROOT'];
-
+var_dump($root_path);
+var_dump(__DIR__);
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
 $root_url = '';
